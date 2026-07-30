1c30efe7cb8351b103d831f869d8d901fb626457
hakeem <abdhakeem123abd@gmail.com>
2026-07-30 10:24:55 +0530

    disable vite-pwa dev options in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       devOptions: {
-        enabled: true
+        enabled: false
       },
       manifest: {
         name: 'Food-Q POS & Management',
